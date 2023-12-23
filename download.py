752921fa699b99ca0f5b5bf154792ce8ae3e3656
Martin Borbola <mborbola@gmail.com>
2023-12-23 18:43:44 +0100

    Create ipynb file on download

--- download.py
+++ download.py
@@ -6,57 +6,64 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def download_url_content(url, cookies):
     try:
         response = requests.get(url, cookies=cookies)
-        response.raise_for_status() 
+        response.raise_for_status()
         return response.text
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
 
+
 def main(year, day_range):
     # Parse the day range argument
-    if '-' in day_range:
-        start_day, end_day = map(int, day_range.split('-'))
+    if "-" in day_range:
+        start_day, end_day = map(int, day_range.split("-"))
     else:
         start_day = end_day = int(day_range)
 
-    cookies = {'session': os.getenv('SESSION_ID')}
+    cookies = {"session": os.getenv("SESSION_ID")}
 
-    for day in range(start_day, end_day+1):
+    for day in range(start_day, end_day + 1):
         exercise_url = f"https://adventofcode.com/{year}/day/{day}"
         exercise_html = download_url_content(exercise_url, cookies)
-        soup = BeautifulSoup(exercise_html, 'html.parser')
+        soup = BeautifulSoup(exercise_html, "html.parser")
 
         exercise_texts = []
 
-        article_tags = soup.findAll('article', {'class': "day-desc"})
+        article_tags = soup.findAll("article", {"class": "day-desc"})
         for tag in article_tags:
-            for h2_tag in tag.select("h2"): 
+            for h2_tag in tag.select("h2"):
                 h2_tag.extract()
-            text = tag.getText() 
+            text = tag.getText()
             exercise_texts.append(text)
 
         input_url = f"https://adventofcode.com/{year}/day/{day}/input"
         input_text = download_url_content(input_url, cookies)
 
-        dirname = str(day) # directory name for day
-        if not os.path.exists(dirname): # check if directory exists
+        dirname = str(day)  # directory name for day
+        if not os.path.exists(dirname):  # check if directory exists
             try:
-                os.makedirs(dirname) # create it if it doesn't exist
+                os.makedirs(dirname)  # create it if it doesn't exist
             except OSError as e:
                 print(e)
 
         for index, exercise_text in enumerate(exercise_texts):
             exercise_filename = f"Part_{index+1}.txt"
-            path = os.path.join(dirname, exercise_filename)
-            with open(path, "w") as file:
+            exercise_path = os.path.join(dirname, exercise_filename)
+            with open(exercise_path, "w") as file:
                 file.writelines(exercise_text)
-        
+
         input_filename = "input.txt"
-        path = os.path.join(dirname, input_filename)
-        with open(path, "w") as file:
+        input_path = os.path.join(dirname, input_filename)
+        with open(input_path, "w") as file:
             file.writelines(input_text)
+        nb_filename = f"{str(day)}.ipynb"
+        nb_path = os.path.join(dirname, nb_filename)
+        with open(nb_path, "w") as file:
+            pass
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
